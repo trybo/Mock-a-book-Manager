12a51cffd785b7a988a1dc550f36ae1afe23f231
Paweł Czachura <trybo1234@gmail.com>
2021-04-17 20:22:01 +0200

    Added footer component

---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mt-5">
+    <h4 class="font-weight-bold">Build with</h4>
+    <div class="text-center">
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="../assets/logo-vuejs.png" width="60"
+      /></a>
+      <a href="https://www.mockaroo.com/" target="_blank"><img src="../assets/logo-mockaroo.png" width="60" /></a>
+      <a href="https://www.chartjs.org/" target="_blank"><img src="../assets/logo-chartjs.svg" width="60" /></a>
+      <a href="https://getbootstrap.com/" target="_blank"><img src="../assets/logo-bootstrap.png" width="60" /></a>
+    </div>
+  </div>
+</template>
+
