c99dbfdb5c9486c309de16a9d4d53477e86f1f93
Paweł Czachura <trybo1234@gmail.com>
2021-04-17 13:03:15 +0200

    Added BaseButton and replace it to pages

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col text-center my-3">
+      <button class="btn btn-purple btn-lg" type="button">
+        <slot></slot>
+      </button>
+    </div>
+</template>
+
+<style scoped>
+.btn {
+  background: #802bb1;
+  color: #d1d7e0;
+  width: 250px;
+}
+
+.btn:hover {
+  background: #702bb1;
+  color: #e1d7e0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import TheHeader from './components/TheHeader.vue'
+import BaseButton from './components/BaseButton.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,5 +13,6 @@ const app = createApp(App)
 app.use(router);
 
 app.component('TheHeader', TheHeader);
+app.component('BaseButton', BaseButton);
 
 app.mount('#app');
