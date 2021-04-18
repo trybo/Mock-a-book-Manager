a0edf91dd45b090a2acde6c4c851871cb39bd63e
Maciej Czarnik <maciek.czarn@gmail.com>
2021-04-18 11:05:22 +0200

    Added pie GenreChart

---
+++ src/views/ChartContainer.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h1>GenreChart</h1>
+    <GenreChart
+      v-bind:chartData="this.state.chartData"
+      v-bind:chartOptions="this.state.chartOptions"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import GenreChart from "./Chart";
+
+export default defineComponent({
+  name: "GenreChartContainer",
+  components: { GenreChart },
+  data() {
+    return {
+      api: "https://my.api.mockaroo.com",
+      api_key: "5ffd1860",
+      state: {
+        genres: [],
+        chartData: {},
+        chartOptions: {
+          responsive: true,
+        },
+      },
+    };
+  },
+  mounted() {},
+  beforeMount() {
+    this.getApi();
+  },
+  methods: {
+    getApi() {
+      fetch(`${this.api}/books_project2.json?key=${this.api_key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          result.forEach((element) => {
+            typeof element.book.genre;
+            if (this.state.genres[element.book.genre]) {
+              this.state.genres[element.book.genre] += 1;
+            } else {
+              this.state.genres[element.book.genre] = 1;
+            }
+          });
+        });
+
+      console.log(this.state.genres["Adventure"]);
+
+      this.state.chartData = {
+        labels: this.state.genre,
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [1, 2], //this.state.genres.keys(),
+            borderColor: "#80b6f4",
+            pointBorderColor: "#80b6f4",
+            pointBackgroundColor: "#80b6f4",
+            pointHoverBackgroundColor: "#80b6f4",
+            pointHoverBorderColor: "#80b6f4",
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 3,
+            fill: true,
+            borderWidth: 4,
+          },
+        ],
+      };
+    },
+  },
+});
+</script>  
